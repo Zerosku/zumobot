83fd948daa32601643216ad29ed57025c63f08bb
Oskari Tamminen <Oskari Tamminen>
2017-02-16 13:42:55 +0200

    valmis?

--- ZumoBot.cydsn/main.c
+++ ZumoBot.cydsn/main.c
@@ -1,34 +1,3 @@
-/**
-* @mainpage ZumoBot Project
-* @brief    You can make your own ZumoBot with various sensors.
-* @details  <br><br>
-    <p>
-    <B>General</B><br>
-    You will use Pololu Zumo Shields for your robot project with CY8CKIT-059(PSoC 5LP) from Cypress semiconductor.This 
-    library has basic methods of various sensors and communications so that you can make what you want with them. <br> 
-    <br><br>
-    </p>
-    
-    <p>
-    <B>Sensors</B><br>
-    &nbsp;Included: <br>
-        &nbsp;&nbsp;&nbsp;&nbsp;LSM303D: Accelerometer & Magnetometer<br>
-        &nbsp;&nbsp;&nbsp;&nbsp;L3GD20H: Gyroscope<br>
-        &nbsp;&nbsp;&nbsp;&nbsp;Reflectance sensor<br>
-        &nbsp;&nbsp;&nbsp;&nbsp;Motors
-    &nbsp;Wii nunchuck<br>
-    &nbsp;TSOP-2236: IR Receiver<br>
-    &nbsp;HC-SR04: Ultrasonic sensor<br>
-    &nbsp;APDS-9301: Ambient light sensor<br>
-    &nbsp;IR LED <br><br><br>
-    </p>
-    
-    <p>
-    <B>Communication</B><br>
-    I2C, UART, Serial<br>
-    </p>
-*/
-
 #include <project.h>
 #include <stdio.h>
 #include "Motor.h"
@@ -45,184 +14,6 @@
 
 int rread(void);
 
-/**
- * @file    main.c
- * @brief   
- * @details  ** You should enable global interrupt for operating properly. **<br>&nbsp;&nbsp;&nbsp;CyGlobalIntEnable;<br>
-*/
-
-
-//battery level//
-/*int main()
-{
-    CyGlobalIntEnable; 
-    UART_1_Start();
-    ADC_Battery_Start();        
-    int16 adcresult =0;
-    float volts = 0.0;
-    
-    printf("\nBoot\n");
-
-    //BatteryLed_Write(1); // Switch led on 
-    BatteryLed_Write(0); // Switch led off 
-    //uint8 button;
-    //button = SW1_Read(); // read SW1 on pSoC board
-
-    
-   
-   
-    
-    
-    
-    for(;;)
-    {
-        
-        ADC_Battery_StartConvert();
-        if(ADC_Battery_IsEndConversion(ADC_Battery_WAIT_FOR_RESULT)) {   // wait for get ADC converted value
-            adcresult = ADC_Battery_GetResult16();
-            volts = ADC_Battery_CountsTo_Volts(adcresult);                  // convert value to Volts
-            float realvolts = volts * 1.5;
-            // If you want to print value
-            printf("%d %f\r\n",adcresult, realvolts);
-            
-            
-            
-        }
-        CyDelay(10000);
-        
-    }
- }   
-//*/
-
-
-/*//ultra sonic sensor//
-int main()
-{
-    CyGlobalIntEnable; 
-    UART_1_Start();
-    Ultra_Start();                          // Ultra Sonic Start function
-    while(1) {
-        //If you want to print out the value  
-        printf("distance = %5.0f\r\n", Ultra_GetDistance());
-        CyDelay(1000);
-    }
-}   
-//*/
-
-
-/*//nunchuk//
-int main()
-{
-    CyGlobalIntEnable; 
-    UART_1_Start();
-  
-    nunchuk_start();
-    nunchuk_init();
-    
-    for(;;)
-    {    
-        nunchuk_read();
-    }
-}   
-//*/
-
-
-/*//IR receiver//
-int main()
-{
-    CyGlobalIntEnable; 
-    UART_1_Start();
-    
-    unsigned int IR_val; 
-    
-    
-    
-    for(;;)
-    {
-       IR_val = get_IR();
-       printf("%x\r\n\n",IR_val);
-    }    
- }   
-//*/
-
-
-/*//Ambient light sensor//
-int main()
-{
-    CyGlobalIntEnable; 
-    UART_1_Start();
-    
-    I2C_Start();
-    
-    I2C_write(0x29,0x80,0x00);          // set to power down
-    I2C_write(0x29,0x80,0x03);          // set to power on
-    
-    for(;;)
-    {    
-        uint8 Data0Low,Data0High,Data1Low,Data1High;
-        Data0Low = I2C_read(0x29,CH0_L);
-        Data0High = I2C_read(0x29,CH0_H);
-        Data1Low = I2C_read(0x29,CH1_L);
-        Data1High = I2C_read(0x29,CH1_H);
-        
-        uint8 CH0, CH1;
-        CH0 = convert_raw(Data0Low,Data0High);      // combine Data0
-        CH1 = convert_raw(Data1Low,Data1High);      // combine Data1
-
-        double Ch0 = CH0;
-        double Ch1 = CH1;
-        
-        double data = 0;
-        data = getLux(Ch0,Ch1);
-        
-        // If you want to print out data
-        //printf("%lf\r\n",data);    
-    }    
- }   
-//*/
-
-
-/*//accelerometer//
-int main()
-{
-    CyGlobalIntEnable; 
-    UART_1_Start();
-  
-    I2C_Start();
-  
-    uint8 X_L_A, X_H_A, Y_L_A, Y_H_A, Z_L_A, Z_H_A;
-    int16 X_AXIS_A, Y_AXIS_A, Z_AXIS_A;
-    
-    I2C_write(ACCEL_MAG_ADDR, ACCEL_CTRL1_REG, 0x37);           // set accelerometer & magnetometer into active mode
-    I2C_write(ACCEL_MAG_ADDR, ACCEL_CTRL7_REG, 0x22);
-    
-    
-    for(;;)
-    {
-        //print out accelerometer output
-        X_L_A = I2C_read(ACCEL_MAG_ADDR, OUT_X_L_A);
-        X_H_A = I2C_read(ACCEL_MAG_ADDR, OUT_X_H_A);
-        X_AXIS_A = convert_raw(X_L_A, X_H_A);
-        
-        Y_L_A = I2C_read(ACCEL_MAG_ADDR, OUT_Y_L_A);
-        Y_H_A = I2C_read(ACCEL_MAG_ADDR, OUT_Y_H_A);
-        Y_AXIS_A = convert_raw(Y_L_A, Y_H_A);
-        
-        Z_L_A = I2C_read(ACCEL_MAG_ADDR, OUT_Z_L_A);
-        Z_H_A = I2C_read(ACCEL_MAG_ADDR, OUT_Z_H_A);
-        Z_AXIS_A = convert_raw(Z_L_A, Z_H_A);
-        
-        printf("ACCEL: %d %d %d %d %d %d \r\n", X_L_A, X_H_A, Y_L_A, Y_H_A, Z_L_A, Z_H_A);
-        value_convert_accel(X_AXIS_A, Y_AXIS_A, Z_AXIS_A);
-        printf("\n");
-        
-        CyDelay(50);
-    }
-}   
-//*/
-
-
-//reflectance//
 int main()
 {
     struct sensors_ ref;
@@ -232,14 +23,11 @@ int main()
   
     int vase;
     int oikee;
-    int flip = 0;
-    int flip2 = 0;
+    int speedmax = 254;
     double speedOtupla;
     double speedVtupla;
-    
-    
-    CyDelay(3000);
-    
+    int mustaviiva = 1;
+
     //starters
     
     reflectance_start();
@@ -281,6 +69,7 @@ int main()
             }
     }*/
     
+    
     for(;;)
     {
         reflectance_read(&ref);
@@ -288,29 +77,21 @@ int main()
         reflectance_digital(&dig);      //print out 0 or 1 according to results of reflectance period
        // printf("%d %d %d %d \r\n", dig.l3, dig.l1, dig.r1, dig.r3);       //print out 0 or 1 according to results of reflectance period
         
-       
-        
-        /*if(ref.l1>=ref.r1){
-            flip++;
-            flip2++;
-        }else if (flip2>500){
-            flip=0;
-        }*/
-        
-        
-        //printf("%i\n",flip);
+        CyDelay(5);
         
         // matikkaa
-        oikee = ref.r1;
-        vase = ref.l1;
+        oikee = ref.r1 + 500;
+        vase = ref.l1 + 500;
         
-        if (oikee<=12000){
+
+        if (oikee<=11000){
             oikee=ref.r1/2;
         }
-        if (vase<=12000){
+        if (vase<=11000){
             vase=ref.l1/2;
         }
         
+        //toisen asteen yhtälöä käytetty jotta zumo osaisi kiihdyttää ja jarruttaa todella nätisti
         double speedO = ((0.391571*oikee*oikee)-(3.84672*oikee)-227.19)/1000000;
         double speedV = ((0.391571*vase*vase)-(3.84672*vase)-227.19)/1000000;
         
@@ -318,152 +99,76 @@ int main()
         int speedVtupla = (int)speedV;
 
         
-        // ei anna yksittäisen moottorin nopeuden nousta yli 255
-        if (speedO >=254){
-            speedO=255;
+        // asettaa huippunopeuden päälle kun tietyt kriteerit täyttyvät
+        if (speedO >=210){
+            speedO=speedmax;
             printf("1");
         }
-        if (speedV >=254){
-            speedV=255;
+        if (speedV >=210){
+            speedV=speedmax;
             printf("2");
         }
-        
-        // ajaa täysiä eteenpäin jos molemmat moottorit ovat nopeudelta yli 240
-        if (speedV >200 && speedO>200){
-            speedV=255;
-            speedO=255;
-            printf("3");
+        if (speedV >=200 && speedO >=200){
+            speedO=speedmax;
+            speedV=speedmax;
         }
         
         
-        //jos toisen moottorin nopeus laskee alle 5, niin antaa sen nopeuden + 250 nopeutta vastakkaiselle puolelle
-        if (speedO < 5) {
-            speedV = 250+speedO;
-            printf("4");
+        //jos toisen moottorin nopeus laskee tietyn nopeuden alle niin lisää toiseen moottoriin nopeutta.
+        if (oikee < 30) {
+            speedV = 220+speedO;
+            speedO = 0;
         }
-        if (speedV < 5) {
-            speedO = 250+speedV;
-            printf("5");
+        if (vase < 30) {
+            speedO = 220+speedV;
+            speedV = 0;
+        }
+        if (oikee < 80) {
+            speedV = speedmax;
+                    }
+        if (vase < 80) {
+            speedO = speedmax;
+              
         }
         
-        //antaa pienen tönäisyn jos jostain syystä molemmat moottorit hidastuvat alle 5
-
+        // hätätilanteisiin joissa robotti eksyy radalta, käytämme reunimmaisia antureita
+        if (ref.r3 > 6000 && dig.l1 == 1 && dig.l3 == 1){
+            speedO = speedmax;
+            speedV = 15;
+        }
+        if (dig.r3 == 1 && dig.r1 == 1 && ref.l3 > 6000){
+            speedV = speedmax;
+            speedO = 15;
+        }
+        if (ref.l3 > 13000 && ref.l3 < 8000){
+            motor_turboturnLeft(255,255);
+        }
+        if (ref.r3 > 13000 && ref.r3 < 8000){
+            motor_turboturnRight(255,255);
+        }
+        
+        // pääasiallinen ajaminen
              motor_turn(speedO,speedV,0);
-            if (speedV < 5 && speedO < 5) {
             
-            motor_forward(100,10);
-            printf("3");
+        // viivoille pysähtyminen ja ohjaimella lähettäminen
+        if(dig.l3 == 0 && dig.l1== 0 && dig.r1== 0 && dig.r3== 0){
+        
+            if(mustaviiva==1){
+                motor_stop();
+                wait_going_down();
+                motor_start();
+                motor_turn(speedO,speedV,0);
+            }else if(mustaviiva == 3){
+                motor_stop();
         }
-        /*if (flip>500){
-            
-            motor_turn(speedO,speedV,0);
-        }else{
-            motor_turn(speedV,speedO,0);
-        }*/
+        mustaviiva++;
+        CyDelay(92);
     }
+
+    }
+    
 }
-//
 
-  /*//motor//
-int main()
-{
-    CyGlobalIntEnable; 
-    UART_1_Start();
-
-    motor_start();              // motor start
-
-    motor_forward(100,2000);     // moving forward
-    motor_turn(200,50,2000);     // turn vasen 200 oikee 50
-    motor_turn(50,200,2000);     // turn oikee 200 vasen 50
-    motor_backward(100,2000);    // movinb backward
-       
-    motor_stop();               // motor stop
-    
-    for(;;)
-    {
-
-    }
-}
-//*/
-    
-
-/*//gyroscope//
-int main()
-{
-    CyGlobalIntEnable; 
-    UART_1_Start();
-  
-    I2C_Start();
-  
-    uint8 X_L_G, X_H_G, Y_L_G, Y_H_G, Z_L_G, Z_H_G;
-    int16 X_AXIS_G, Y_AXIS_G, Z_AXIS_G;
-    
-    I2C_write(GYRO_ADDR, GYRO_CTRL1_REG, 0x0F);             // set gyroscope into active mode
-    I2C_write(GYRO_ADDR, GYRO_CTRL4_REG, 0x30);             // set full scale selection to 2000dps    
-    
-    for(;;)
-    {
-        //print out gyroscope output
-        X_L_G = I2C_read(GYRO_ADDR, OUT_X_AXIS_L);
-        X_H_G = I2C_read(GYRO_ADDR, OUT_X_AXIS_H);
-        X_AXIS_G = convert_raw(X_H_G, X_L_G);
-        
-        
-        Y_L_G = I2C_read(GYRO_ADDR, OUT_Y_AXIS_L);
-        Y_H_G = I2C_read(GYRO_ADDR, OUT_Y_AXIS_H);
-        Y_AXIS_G = convert_raw(Y_H_G, Y_L_G);
-        
-        
-        Z_L_G = I2C_read(GYRO_ADDR, OUT_Z_AXIS_L);
-        Z_H_G = I2C_read(GYRO_ADDR, OUT_Z_AXIS_H);
-        Z_AXIS_G = convert_raw(Z_H_G, Z_L_G);
-     
-        // If you want to print value
-        printf("%d %d %d \r\n", X_AXIS_G, Y_AXIS_G, Z_AXIS_G);
-        CyDelay(50);
-    }
-}   
-//*/
-
-
-/*//magnetometer//
-int main()
-{
-    CyGlobalIntEnable; 
-    UART_1_Start();
-  
-    I2C_Start();
-   
-    uint8 X_L_M, X_H_M, Y_L_M, Y_H_M, Z_L_M, Z_H_M;
-    int16 X_AXIS, Y_AXIS, Z_AXIS;
-    
-    I2C_write(GYRO_ADDR, GYRO_CTRL1_REG, 0x0F);             // set gyroscope into active mode
-    I2C_write(GYRO_ADDR, GYRO_CTRL4_REG, 0x30);             // set full scale selection to 2000dps
-    I2C_write(ACCEL_MAG_ADDR, ACCEL_CTRL1_REG, 0x37);           // set accelerometer & magnetometer into active mode
-    I2C_write(ACCEL_MAG_ADDR, ACCEL_CTRL7_REG, 0x22);
-    
-    
-    for(;;)
-    {
-        X_L_M = I2C_read(ACCEL_MAG_ADDR, OUT_X_L_M);
-        X_H_M = I2C_read(ACCEL_MAG_ADDR, OUT_X_H_M);
-        X_AXIS = convert_raw(X_L_M, X_H_M);
-        
-        Y_L_M = I2C_read(ACCEL_MAG_ADDR, OUT_Y_L_M);
-        Y_H_M = I2C_read(ACCEL_MAG_ADDR, OUT_Y_H_M);
-        Y_AXIS = convert_raw(Y_L_M, Y_H_M);
-        
-        Z_L_M = I2C_read(ACCEL_MAG_ADDR, OUT_Z_L_M);
-        Z_H_M = I2C_read(ACCEL_MAG_ADDR, OUT_Z_H_M);
-        Z_AXIS = convert_raw(Z_L_M, Z_H_M);
-        
-        heading(X_AXIS, Y_AXIS);
-        printf("MAGNET: %d %d %d %d %d %d \r\n", X_L_M, X_H_M, Y_L_M, Y_H_M, Z_L_M, Z_H_M);
-        printf("%d %d %d \r\n", X_AXIS,Y_AXIS, Z_AXIS);
-        CyDelay(50);      
-    }
-}   
-//*/
 
 
 #if 0
